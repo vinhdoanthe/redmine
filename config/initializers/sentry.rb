575ed6a370f987b0721d54c9f6b26751028469e8
Đoàn Thế Vinh <vinhdt@teky.edu.vn>
2021-04-26 01:42:45 +0700

    Update Sentry initializer

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
 Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
-  config.breadcrumbs_logger = [:active_support_logger]
+  config.dsn = 'https://b62b8db3c97045279ce88070e5a0680f@o487682.ingest.sentry.io/5735224'
+
+#  config.dsn = ENV["SENTRY_DSN"]
+#  config.breadcrumbs_logger = [:active_support_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
